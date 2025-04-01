0a1407dcc2577a72d77f643bbaad1b21c679f451
Benjamin Bernacle <bernaczekpatryk@gmail.com>
2025-04-01 10:58:10 +0200

    czat gpt

--- Benjamin Bernacle/arkuszeINF08/czerwiec2021.cpp
+++ Benjamin Bernacle/arkuszeINF08/czerwiec2021.cpp
@@ -1,37 +1,51 @@
 #include <iostream>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) {
+class Sort {
+public:
+  
+    int findMinIndex(int arr[], int start, int n) {
+        int minIndex = start;
+        for (int j = start + 1; j < n; j++) {
             if (arr[j] < arr[minIndex]) {
                 minIndex = j;
             }
         }
-        swap(arr[i], arr[minIndex]);
+        return minIndex;
     }
-}
 
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+    
+    void selectionSort(int arr[], int n) {
+        for (int i = 0; i < n - 1; i++) {
+           
+            int minIndex = findMinIndex(arr, i, n);
+           
+            swap(arr[i], arr[minIndex]);
+        }
     }
-    cout << endl;
-}
+
+    
+    void printArray(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+};
 
 int main() {
     int arr[10];
     int n = 10;
-    
+    Sort sortowanie;
+
     cout << "Podaj 10 liczb calkowitych: ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    
-    selectionSort(arr, n);
+
+    sortowanie.selectionSort(arr, n);
     cout << "Posortowana tablica: ";
-    printArray(arr, n);
-    
+    sortowanie.printArray(arr, n);
+
     return 0;
 }
