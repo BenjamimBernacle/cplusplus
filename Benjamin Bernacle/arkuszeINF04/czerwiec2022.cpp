65360afaa262ab5d4cd96246e751f8508d4983e8
Benjamin Bernacle <bernaczekpatryk@gmail.com>
2025-04-02 08:59:20 +0200

    sperma

--- Benjamin Bernacle/arkuszeINF04/czerwiec2022.cpp
+++ Benjamin Bernacle/arkuszeINF04/czerwiec2022.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 void wypelnijTablice(int arr[], int n) {
     for (int i = 0; i < n; i++) {
-    srand(time(NULL) * (i + 1));
     arr[i] = (rand() %  100 + 1); // Wypełnienie pseudolosowymi wartościami 1-100
     }
 }
@@ -13,25 +12,26 @@ void wypelnijTablice(int arr[], int n) {
 
 int przeszukajTablice(int arr[], int n, int x) {
     arr[n] = x; // Dodanie wartownika
-    int i = 0;
-    while (arr[i] != x) {
-        i++;
+    int i;
+    for (i = 0; arr[i] != x; i++);
+    if (i == n) {
+        return -1;
     }
-    return (i == n) ? -1 : i; // Sprawdzenie, czy znaleziono w oryginalnej tablicy
+    return i;
 }
 
 void wyswietlTablice(int arr[], int n) {
     cout << "Tablica zawiera następujące liczby: ";
     for (int i = 0; i < n; i++) {
         cout << arr[i];
-        if (i < n - 1) cout << ", ";
+     cout << ", ";
     }
     cout << endl;
 }
 
 int main() {
-    const int ROZMIAR = 50;
-    int arr[ROZMIAR + 1]; // Powiększona tablica na wartownika
+    const int ROZMIAR = 51;
+    int arr[ROZMIAR]; // Powiększona tablica na wartownika
     
     cout << "Program wyszukuje liczbę w 50-elementowej tablicy losowych liczb z zakresu 1-100.\n";
     wypelnijTablice(arr, ROZMIAR); // Wypełnienie tablicy
